1a796dc3a6738e2c89ba28a824105b8eb4fed049
Arijit7828 <143728831+Arijit7828@users.noreply.github.com>
2023-11-23 21:15:21 +0530

    n

---
+++ home10.c
@@ -0,0 +1,16 @@
+//
+// Created by dutta on 23-11-2023.
+//
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int age;
+    l1:
+    printf("enter the age\n");
+    scanf("%d",&age);
+    if(age>=18){
+        exit(0);
+    }else{
+        goto l1;
+    }
+}
