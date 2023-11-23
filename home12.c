b008cc85cfb6fa81d94184468e70d851e630f0c0
Arijit7828 <143728831+Arijit7828@users.noreply.github.com>
2023-11-23 21:43:00 +0530

    n

---
+++ home12.c
@@ -0,0 +1,34 @@
+//
+// Created by dutta on 23-11-2023.
+//
+#include<stdio.h>
+
+int sum_n(int n){
+    printf("program to find the sum of n numbers\n");
+    int sum=0;
+    for(int i=0;i<=n;i++){
+        sum=sum+1;
+    }
+    printf("the sum of numbers are: %d\n",sum);
+}
+int mul_n(int n){
+    printf("program to find the multiplication of n\n");
+    for(int i=1;i<n;i++){
+        for(int j=i;j<=10;j++){
+            printf("%d * %d=%d\n", i,j,i*j);
+        }
+    }
+}
+int main(){
+    int n,choice;
+    printf("entter the value of n:\n");
+    scanf("%d",&n);
+    printf("enter your choice\n");
+    scanf("%d",&choice);
+
+    if(choice==0){
+        sum_n(n);
+    }else if(choice==1){
+        mul_n(n);
+    }
+}
